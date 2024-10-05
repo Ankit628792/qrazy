97261ec6f8f069c0e7eb5571a116b51b4d606e95
am1t-kumar <thorragnarak@gmail.com>
2024-10-06 03:39:25 +0530

    ref: code ref

--- src/components/templates/layouts/sidebar-layout/index.tsx
+++ src/components/templates/layouts/sidebar-layout/index.tsx
@@ -4,18 +4,21 @@ import { SidebarLayoutProps } from '@/types'
 import { Container } from '@/components/atoms'
 import { LeftSideBarMenu } from '@/shared/components/left-sidebar'
 import { useLocalStorage } from '@/components/hooks/useLocalStorage'
-import { AUTH_LOCAL_STORAGE_KEYS } from '@/features/auth/auth.interface'
+import {
+  AUTH_LOCAL_STORAGE_KEYS,
+  PERMISSION_LEVEL
+} from '@/features/auth/auth.interface'
 
 export const SidebarLayout = ({ children }: SidebarLayoutProps) => {
   const { setItem: setModulesAndPermissions } = useLocalStorage(
     AUTH_LOCAL_STORAGE_KEYS.MODULES_AND_PERMISSIONS
   )
   const permissions = {
-    '/user': 'denied',
-    '/invoice': 'denied',
-    '/product': 'denied',
-    '/report': 'denied',
-    '/overview': 'denied'
+    '/user': PERMISSION_LEVEL.DENIED,
+    '/invoice': PERMISSION_LEVEL.DENIED,
+    '/product': PERMISSION_LEVEL.DENIED,
+    '/report': PERMISSION_LEVEL.DENIED,
+    '/overview': PERMISSION_LEVEL.DENIED
   }
   setModulesAndPermissions(JSON.stringify(permissions))
   return (

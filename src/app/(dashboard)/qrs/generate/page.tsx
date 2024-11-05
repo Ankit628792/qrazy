51ac77c45e40ad647253315cbeba03c0914ba2f5
Ankit Kumar <ankit628792@mail.com>
2024-11-05 19:35:51 +0530

    dyn import

--- src/app/(dashboard)/qrs/generate/page.tsx
+++ src/app/(dashboard)/qrs/generate/page.tsx
@@ -1,5 +1,10 @@
-import QRGenerate from '@/components/qrs/generate'
+"use client"
+// import QRGenerate from '@/components/qrs/generate'
 import React from 'react'
+import dynamic from 'next/dynamic';
+
+const QRGenerate = dynamic(() => import('@/components/qrs/generate'), { ssr: false });
+
 
 function Page() {
     return (
